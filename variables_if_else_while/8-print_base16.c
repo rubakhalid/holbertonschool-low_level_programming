28a5da3966b39cf49f792a41e372b9c7ba5cc310
rubakhalid <rubak.alhumaid@gmail.com>
2025-02-18 12:02:50 +0000

    new

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+
+{
+
+int i;
+char ch;
+
+for (i = 0; i <= 9; i++)
+{
+putchar(i + '0');
+}
+
+for (ch = 'a'; ch <= 'f'; ch++)
+{
+putchar(ch);
+}
+
+putchar('\n');
+return (0);
+
+}
