86bd7449337eefc4276ea412e782396ffff67f58
rubakhalid <rubak.alhumaid@gmail.com>
2025-02-19 11:16:45 +0000

    10 alp

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
-/**                                                                                        
-* print_alphabet - Prints the lowercase alphabet followed by a new line                    
-*                                                                                          
-* Description: This function prints the English alphabet in lowercase,                     
-*              followed by a new line. It uses the _putchar function                       
-*              to print each character.                                                    
+/**
+*print_alphabet - Prints the lowercase alphabet followed by a new line
+*
+* Description: This function prints the English alphabet in lowercase,
+*followed by a new line. It uses the _putchar function
+*to print each character.
 */
 
 void print_alphabet_x10(void)
@@ -13,7 +13,7 @@ void print_alphabet_x10(void)
 char letter;
 int i;
 
-for ( i = 0; i < 10; i++)
+for (i = 0; i < 10; i++)
 {
 for (letter = 'a'; letter <= 'z'; letter++)
 {
