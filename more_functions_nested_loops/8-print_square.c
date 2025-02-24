a450e424e48ef6c85b1820c46ec03613e52de360
rubakhalid <rubak.alhumaid@gmail.com>
2025-02-24 16:20:29 +0000

    update

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+/**
+ * print_square - Draws a square in the terminal.
+ * @size: The size of the square to be printed.
+ */
+
+void print_square(int size)
+{
+int i, j;
+
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+void print_square(int size);
+
 void print_diagonal(int n);
 void print_line(int n);
 
