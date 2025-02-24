7bc82b64d10924c636b24d108d8d8f8794cf4b15
rubakhalid <rubak.alhumaid@gmail.com>
2025-02-24 17:04:35 +0000

    update

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle of a given size
+ * @size: The size of the triangle
+ *
+ */
+
+
+void print_triangle(int size)
+{
+
+int i, j, k;
+
+
+
+if (size <= 0)
+{
+_putchar('\n');
+return;
+}
+for (i = 1; i <= size; i++)
+{
+for (j = 1; j <= size - i; j++)
+{
+_putchar(' ');
+}
+for (k = 1; k <= i; k++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+
+void print_triangle(int size);
+
 void print_square(int size);
 
 void print_diagonal(int n);
