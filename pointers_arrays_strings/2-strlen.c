c95bee4a6c0ec20ec8561e9e7ba46d136b95bfab
rubakhalid <rubak.alhumaid@gmail.com>
2025-02-25 11:00:45 +0000

    update

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -2,10 +2,14 @@
 #include <stdio.h>
 
 /**
-* string_length - Returns the length of a string
-* @str: Pointer to the string (character array)
-*
-*/
+ * _strlen - Returns the length of a string
+ * @s: Pointer to the string (character array)
+ *
+ * This function takes a string as input and counts the number
+ * of characters in the string until the null terminator is reached.
+ *
+ * Return: The length of the string as an integer.
+ */
 
 int _strlen(char *s)
 {
