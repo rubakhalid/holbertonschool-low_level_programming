e9b5597fb7d9338d56bb7dfb8ab72249e7465749
rubakhalid <rubak.alhumaid@gmail.com>
2025-03-02 20:41:08 +0000

    up

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -2,28 +2,28 @@
 #include <stddef.h>  /* For NULL */
 
 /**
- * _strchr - Locates a character in a string
- * @s: The string to search
- * @c: The character to locate
- *
- * Return: A pointer to the first occurrence of c in s, or NULL if not found
- */
+* _strchr - Locates a character in a string
+* @s: The string to search
+* @c: The character to locate
+*
+* Return: A pointer to the first occurrence of c in s, or NULL if not found
+*/
 char *_strchr(char *s, char c)
 {
- while (*s != '\0')  /* Loop through the string until the null terminator */
- {
-  if (*s == c)  /* If the character matches, return a pointer to it */
-  {
-   return (s);
-  }
-  s++;  /* Move to the next character */
- }
+while (*s != '\0')  /* Loop through the string until the null terminator */
+{
+if (*s == c)  /* If the character matches, return a pointer to it */
+{
+return (s);
+}
+s++;  /* Move to the next character */
+}
 
- /* If c is the null character, return a pointer to the null terminator */
- if (c == '\0')
- {
-  return (s);
- }
+/* If c is the null character, return a pointer to the null terminator */
+if (c == '\0')
+{
+return (s);
+}
 
- return (NULL);  /* Character not found, return NULL */
+return (NULL);  /* Character not found, return NULL */
 }
