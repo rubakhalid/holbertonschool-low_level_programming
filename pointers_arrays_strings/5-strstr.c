3863935ce4889284d8d3257009fb183bf54434cb
rubakhalid <rubak.alhumaid@gmail.com>
2025-03-03 20:57:41 +0000

    up

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stddef.h>
+/**
+* _strstr - Locates the first occurrence of needle in haystack
+* @haystack: String to search in
+* @needle: Substring to find
+*
+* Return: Pointer to the start of needle in haystack, or NULL if not found
+*/
+char *_strstr(char *haystack, char *needle) {
+if (!*needle)
+return haystack;
+
+while (*haystack) {
+char *h = haystack;
+char *n = needle;
+
+while (*h && *n && (*h == *n)) {
+h++;
+n++;
+}
+
+if (!*n)
+return haystack;
+
+haystack++;
+}
+
+return (NULL);
+}
+
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,5 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+
+char *_strstr(char *haystack, char *needle);
+
+
 char *_strpbrk(char *s, char *accept);
 
 unsigned int _strspn(char *s, char *accept);
