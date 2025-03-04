ae4d866076c7faa728b49b32b653356b920f877f
rubakhalid <rubak.alhumaid@gmail.com>
2025-03-04 19:53:07 +0000

    up

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string using recursion.
+ * @s: The string to be printed.
+ *
+ * Return: void (no return value).
+ */
+
+
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
+_putchar(*s);
+_puts_recursion(s + 1);
+}
