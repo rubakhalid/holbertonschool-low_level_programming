d95b9c5fef8ec5af8d246b709fb8ea5d20e2bac8
rubakhalid <rubak.alhumaid@gmail.com>
2025-03-05 19:51:06 +0000

    up

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * main - Prints the name of the program
+ * @argc: Number of command-line arguments (unused)
+ * @argv: Array of strings containing arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(int argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", argv[0]);
+return (0);
+}
---
+++ argc_argv/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+
+
+
+int main(int argc, char *argv[]);
+
+int _putchar(char c);
+
+
+
+#endif /* MAIN_H */
