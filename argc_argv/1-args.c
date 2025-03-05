2ec12e78a644c9894b007eafff2366f17d7997cf
rubakhalid <rubak.alhumaid@gmail.com>
2025-03-05 20:00:48 +0000

    up

---
+++ argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * main - Prints the name of the program
+ * @argc: Number of command-line arguments
+ * @argv: Array of strings containing arguments (unused)
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(int argc, char *argv[])
+{
+(void) argv;
+printf("%d\n", argc - 1);
+return (0);
+}
