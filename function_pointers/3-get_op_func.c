15f992ba13846d66532c38bdbc2257efce34035d
rubakhalid <rubak.alhumaid@gmail.com>
2025-03-16 19:44:57 +0000

    up

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,17 +1,14 @@
 #include "3-calc.h"
-#include <stddef.h>
-
+#include <string.h> /* For strcmp */
 
 /**
- * get_op_func - Selects the appropriate function based on the operator
- * @s: The operator input
- *
- * Return: The corresponding function or NULL if the operator is unknown
+* get_op_func - Selects the function to perform the operation
+* @s: The operator passed as a string
+* Return: A function pointer to the corresponding operation, or NULL if invalid
 */
-
-
 int (*get_op_func(char *s))(int, int)
 {
+/* Defining an array of structs with operators and corresponding functions */
 op_t ops[] = {
 {"+", op_add},
 {"-", op_sub},
@@ -20,12 +17,16 @@ op_t ops[] = {
 {"%", op_mod},
 {NULL, NULL}
 };
-int i;
+int i = 0;
 
-for (i = 0; ops[i].op != NULL; i++)
+/* Checking if the operator matches one of the ones in ops[] */
+while (ops[i].op != NULL)
 {
-if (*s == *(ops[i].op))
+if (strcmp(ops[i].op, s) == 0)
 return (ops[i].f);
+i++;
 }
+
+/* Return NULL if no match is found */
 return (NULL);
 }
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,22 +1,22 @@
+#include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "3-calc.h"
+
 /**
- * main - Executes arithmetic operations based on user input
- * @argc: The number of command-line arguments
- * @argv: The array containing the command-line arguments
- *
- * Return: 0 on success
- */
+* main - Performs simple operations based on user input
+* @argc: Number of command-line arguments
+* @argv: The command-line arguments
+* Return: 0 on success, or an error code on failure
+*/
 int main(int argc, char *argv[])
 {
-int num1, num2;
+int num1, num2, result;
 int (*op_func)(int, int);
 
 if (argc != 4)
 {
 printf("Error\n");
-exit(98);
+return (98);
 }
 
 num1 = atoi(argv[1]);
@@ -27,15 +27,17 @@ op_func = get_op_func(argv[2]);
 if (op_func == NULL)
 {
 printf("Error\n");
-exit(99);
+return (99);
 }
 
-if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
 {
 printf("Error\n");
-exit(100);
+return (100);
 }
 
-printf("%d\n", op_func(num1, num2));
+result = op_func(num1, num2);
+printf("%d\n", result);
+
 return (0);
 }
