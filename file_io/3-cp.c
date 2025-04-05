398ebcc6b5630046ed92d287e3f17cf3647a0250
rubakhalid <rubak.alhumaid@gmail.com>
2025-04-05 19:19:22 +0000

    up

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,104 +1,130 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
 
 #define BUFFER_SIZE 1024
 
 /**
- * open_source_file - Opens the source file for reading
- * @filename: name of the file to read from
- * Return: file descriptor on success, exits with 98 on failure
- */
-int open_source_file(char *filename)
+* error_exit - An helper function to handle errors print and exit
+*
+* @code: The exit code to return
+* @msg: The error message to be printed
+* @arg: A string of arguments for message
+*
+* Return: Nothing
+*/
+
+void error_exit(int code, char *msg, int arg)
 {
-int fd = open(filename, O_RDONLY);
-if (fd == -1)
-{
-dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", filename);
-exit(98);
-}
-return (fd);
+dprintf(STDERR_FILENO, msg, arg);
+exit(code);
 }
 
 /**
- * open_target_file - Opens or creates the target file for writing
- * @filename: name of the file to write to
- * Return: file descriptor on success, exits with 99 on failure
- */
-int open_target_file(char *filename)
+* error_exit_str - An helper function to print a message with string and exit
+*
+* @code: The exit code to return
+* @msg: The error message to be printed
+* @arg: A string of arguments for message
+*/
+
+void error_exit_str(int code, char *msg, char *arg)
 {
-int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-if (fd == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
-exit(99);
-}
-return (fd);
+dprintf(STDERR_FILENO, msg, arg);
+exit(code);
 }
 
 /**
- * close_file - Closes an open file descriptor
- * @fd: file descriptor to close
- */
-void close_file(int fd)
-{
-if (close(fd) == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-exit(100);
-}
-}
+* copy - Helper function to copy file content
+*
+* @f_f: The source file descriptor
+* @f_t: The destination files descriptor
+* @sr: The source of the file
+* @des: The destination of the file
+* @f_buf: The first buffer read
+* @f_b: The number of bytes inside the first buffer
+*
+* Return: Noting
+*/
 
-/**
- * copy_content - Reads from fd_from and writes to fd_to
- * @fd_from: source file descriptor
- * @fd_to: destination file descriptor
- * @src: source file name
- * @dest: destination file name
- */
-void copy_content(int fd_from, int fd_to, char *src, char *dest)
+void copy(int f_f, int f_t, char *sr, char *des, char *f_buf, ssize_t f_b)
 {
-ssize_t rd, wr;
 char buffer[BUFFER_SIZE];
-while ((rd = read(fd_from, buffer, sizeof(buffer))) > 0)
+ssize_t b_read, b_wr;
+
+b_wr = write(f_t, f_buf, f_b);
+if (b_wr == -1)
 {
-wr = write(fd_to, buffer, rd);
-if (wr < 0 || wr != rd)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest);
-close_file(fd_from);
-close_file(fd_to);
-exit(99);
+close(f_f);
+close(f_t);
+error_exit_str(99, "Error: Can't write to %s\n", des);
 }
-}
-if (rd < 0)
+
+while ((b_read = read(f_f, buffer, BUFFER_SIZE)) > 0)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
-close_file(fd_from);
-close_file(fd_to);
-exit(98);
+b_wr = write(f_t, buffer, b_read);
+if (b_wr == -1)
+{
+close(f_f);
+close(f_t);
+error_exit_str(99, "Error: Can't write to %s\n", des);
 }
 }
 
+if (b_read == -1)
+{
+close(f_f);
+close(f_t);
+error_exit_str(98, "Error: Can't read from file %s\n", sr);
+}
+}
+
+
 /**
- * main - copies the content of a file to another file
- * @ac: argument count
- * @av: argument vector
- * Return: 0 on success
- */
-int main(int ac, char **av)
+* main - Entry point
+*
+* @argc: Argument counts
+* @argv: Values of arguments
+*
+* Return: 0 for success, various code for failures
+*/
+
+int main(int argc, char *argv[])
 {
 int fd_from, fd_to;
-if (ac != 3)
+char buffer[BUFFER_SIZE];
+ssize_t bytes_read;
+
+if (argc != 3)
+error_exit_str(97, "Usage: cp file_from file_to\n", "");
+
+fd_from = open(argv[1], O_RDONLY);
+if (fd_from == -1)
+error_exit_str(98, "Error: Can't read from file %s\n", argv[1]);
+
+bytes_read = read(fd_from, buffer, BUFFER_SIZE);
+if (bytes_read == -1)
 {
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-exit(97);
+close(fd_from);
+error_exit_str(98, "Error: Can't read from file %s\n", argv[1]);
 }
-fd_from = open_source_file(av[1]);
-fd_to = open_target_file(av[2]);
-copy_content(fd_from, fd_to, av[1], av[2]);
-close_file(fd_from);
-close_file(fd_to);
+
+fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+if (fd_to == -1)
+{
+close(fd_from);
+error_exit_str(99, "Error: Can't write to %s\n", argv[2]);
+}
+
+copy(fd_from, fd_to, argv[1], argv[2], buffer, bytes_read);
+
+if (close(fd_from) == -1)
+error_exit(100, "Error: Can't close fd %d\n", fd_from);
+if (close(fd_to) == -1)
+error_exit(100, "Error: Can't close fd %d\n", fd_to);
+
 return (0);
 }
